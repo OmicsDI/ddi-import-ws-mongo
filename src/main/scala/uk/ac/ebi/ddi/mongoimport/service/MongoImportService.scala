680e006afb650eff159fa781841a2f704789ddcf
Gaurhari <gaurharidass@gmail.com>
2019-11-29 15:56:21 +0000

    updated imports which are unused.

--- src/main/scala/uk/ac/ebi/ddi/mongoimport/service/MongoImportService.scala
+++ src/main/scala/uk/ac/ebi/ddi/mongoimport/service/MongoImportService.scala
@@ -1,11 +1,10 @@
 package uk.ac.ebi.ddi.mongoimport.service
 
-//import com.mongodb.{BasicDBObject, DB, DBObject}
-//import com.mongodb.casbah.{MongoClient, MongoClientURI, WriteConcern}
 import org.mongodb.scala._
 import uk.ac.ebi.ddi.mongoimport.servicetrait.ImportServiceTrait
 
 object MongoImportService extends ImportServiceTrait {
+
   val mongoClient: MongoClient = MongoClient()
 
   // get handle to "mydb" database
@@ -15,12 +14,10 @@ object MongoImportService extends ImportServiceTrait {
   val collection: MongoCollection[Document] = database.getCollection("test")
 
   def save(data:String): Unit ={
-
     collection.insertOne(Document.apply(data)).subscribe(new Observer[Completed] {
       override def onNext(result: Completed): Unit = println(s"onNext: $result")
       override def onError(e: Throwable): Unit = println(s"onError: $e")
       override def onComplete(): Unit = println("onComplete")
     })
-
   }
 }
