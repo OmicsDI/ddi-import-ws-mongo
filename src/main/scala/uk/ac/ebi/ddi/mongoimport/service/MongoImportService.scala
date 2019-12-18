cc95ed013e4589135a715ca08f67f71237ededed
Gaurhari <gaurharidass@gmail.com>
2019-12-18 16:42:06 +0000

    moved connection functions to others

--- src/main/scala/uk/ac/ebi/ddi/mongoimport/service/MongoImportService.scala
+++ src/main/scala/uk/ac/ebi/ddi/mongoimport/service/MongoImportService.scala
@@ -5,23 +5,6 @@ import uk.ac.ebi.ddi.mongoimport.servicetrait.ImportServiceTrait
 
 object MongoImportService extends ImportServiceTrait {
 
-  val mongoClient: MongoClient = MongoClient()
-
-  // get handle to "mydb" database
-  val database: MongoDatabase = mongoClient.getDatabase("mydb")
-
-  // get a handle to the "test" collection
-  //val collection: MongoCollection[Document] = database.getCollection("test")
-
-  def getDatasetCollection():MongoCollection[Document] = {
-    database.getCollection("test")
-  }
-
-  def getDatabaseCollection():MongoCollection[Document] = {
-    database.getCollection("databases")
-  }
-
-
   def save(data:String, collection: MongoCollection[Document]): Unit ={
     collection.insertOne(Document.apply(data)).subscribe(new Observer[Completed] {
       override def onNext(result: Completed): Unit = println(s"onNext: $result")
