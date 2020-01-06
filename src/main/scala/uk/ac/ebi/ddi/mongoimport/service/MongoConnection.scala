48bcb1406a96d94ef19fd409e39b72dafb82a9ff
Gaurhari <gaurharidass@gmail.com>
2020-01-06 16:39:12 +0000

    updated mongoconnection singleton object

--- src/main/scala/uk/ac/ebi/ddi/mongoimport/service/MongoConnection.scala
+++ src/main/scala/uk/ac/ebi/ddi/mongoimport/service/MongoConnection.scala
@@ -1,5 +1,23 @@
 package uk.ac.ebi.ddi.mongoimport.service
 
-class MongoConnection {
+import org.mongodb.scala.{Document, MongoClient, MongoCollection, MongoDatabase}
+import uk.ac.ebi.ddi.mongoimport.servicetrait.ConnectionTrait
 
+object MongoConnection extends ConnectionTrait{
+
+  val mongoClient: MongoClient = MongoClient("mongodb://ddi_user:tDzDd81J@mongos-hxvm-001.ebi.ac.uk:27017,mongos-hxvm-001.ebi.ac.uk:27017,mongos-hxvm-001.ebi.ac.uk:27017/ddi_db?authSource=admin")
+
+  // get handle to "mydb" database
+  val database: MongoDatabase = mongoClient.getDatabase("ddi_db")
+
+  // get a handle to the "test" collection
+  //val collection: MongoCollection[Document] = database.getCollection("test")
+
+  def getDatasetCollection():MongoCollection[Document] = {
+    database.getCollection("datasets.dataset")
+  }
+
+  def getDatabaseCollection():MongoCollection[Document] = {
+    database.getCollection("databases")
+  }
 }
