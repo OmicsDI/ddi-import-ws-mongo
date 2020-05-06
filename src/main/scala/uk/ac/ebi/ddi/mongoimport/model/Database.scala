0fa52e240cc565b0e5160b9431ab05a15e3b5c06
Gaurhari <gaurharidass@gmail.com>
2020-05-06 19:29:16 +0100

    similars collection data model for retrieving similars data from web service

--- src/main/scala/uk/ac/ebi/ddi/mongoimport/model/Database.scala
+++ src/main/scala/uk/ac/ebi/ddi/mongoimport/model/Database.scala
@@ -1,7 +1,15 @@
 package uk.ac.ebi.ddi.mongoimport.model
 
+import org.bson.types.ObjectId
+
 /*information about all databases to retrieve datasets */
 case class Database(repository:String,
 orcidName:String, urlTemplate:String, accessionPrefix:Array[String],
 title:String, imgAlt:String, sourceUrl:String, image:Array[String],
 source:String, description:String, domain:String, icon:String, databaseName:String)
+
+case class Similars(_class:String, accession: String, database: String, similars: List[DatasetRef])
+
+case class DatasetRef(relationType: String , similarDataset: SimilarDataset)
+
+case class SimilarDataset($ref: String, $id: ObjectId)
