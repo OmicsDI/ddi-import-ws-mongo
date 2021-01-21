52bd9df1902e604dc14045ee635b7c442ce14c54
Gaurhari <gaurharidass@gmail.com>
2021-01-21 00:56:06 +0000

    updated insertion acknowledgement

--- src/main/scala/uk/ac/ebi/ddi/mongoimport/service/MongoImportService.scala
+++ src/main/scala/uk/ac/ebi/ddi/mongoimport/service/MongoImportService.scala
@@ -6,10 +6,12 @@ import uk.ac.ebi.ddi.mongoimport.servicetrait.ImportServiceTrait
 object MongoImportService extends ImportServiceTrait {
 
   def save(data:String, collection: MongoCollection[Document]): Unit ={
+    collection.withWriteConcern(WriteConcern.ACKNOWLEDGED)
+
     collection.insertOne(Document.apply(data)).subscribe(new Observer[Completed] {
       override def onNext(result: Completed): Unit = println(s"onNext: $result")
       override def onError(e: Throwable): Unit = println(s"onError: $e")
-      override def onComplete(): Unit = println("onComplete")
+      override def onComplete(): Unit = println("insertion successful")
     })
   }
 
