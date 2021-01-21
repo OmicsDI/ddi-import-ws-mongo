aa5dff6a1f0cb72155a4bead2fc0bc61c628fa48
Gaurhari <gaurharidass@gmail.com>
2021-01-21 00:30:13 +0000

    updated database class

--- src/main/scala/uk/ac/ebi/ddi/mongoimport/model/Database.scala
+++ src/main/scala/uk/ac/ebi/ddi/mongoimport/model/Database.scala
@@ -3,7 +3,7 @@ package uk.ac.ebi.ddi.mongoimport.model
 import org.bson.types.ObjectId
 
 /*information about all databases to retrieve datasets */
-case class Database(repository:String,
+case class Database(var _id:Option[String], repository:String,
 orcidName:String, urlTemplate:String, accessionPrefix:Array[String],
 title:String, imgAlt:String, sourceUrl:String, image:Array[String],
 source:String, description:String, domain:String, icon:String, databaseName:String)
