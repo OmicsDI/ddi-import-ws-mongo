c5bff570c6f14ce35ca6298839b77c4bc591b14a
Gaurhari <gaurharidass@gmail.com>
2021-01-21 00:12:55 +0000

    updated field of class

--- src/main/scala/uk/ac/ebi/ddi/mongoimport/model/Dataset.scala
+++ src/main/scala/uk/ac/ebi/ddi/mongoimport/model/Dataset.scala
@@ -1,6 +1,7 @@
 package uk.ac.ebi.ddi.mongoimport.model
 
-case class Dataset (additional: Map[String, Set[String]],
+case class Dataset (var _class:Option[String] = Some("uk.ac.ebi.ddi.service.db.model.dataset.Dataset"),
+                     additional: Map[String, Set[String]],
                    dates: Map[String, Set[String]],
                    accession:String, var database:String,
                    scores:Scores,
